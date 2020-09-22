aa9b38aa34b44fabf380eec8a5be6a3982fef86d
51Joying\YaoTingkai <yaotingkai@51joying.com>
2020-09-22 16:02:04 +0800

    初始化计数器项目的基本结构

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <my-Addition></my-Addition>
+    <p>---------------------------------------</p>
+
+    <my-Subtraction></my-Subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    'my-Addition': Addition,
+    'my-Subtraction': Subtraction
+  }
+}
+</script>
+
+<style></style>
